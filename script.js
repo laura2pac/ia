0a4290d2f4243729c4e3b21a0b146879c59eae48
laura2pac <104081738+laura2pac@users.noreply.github.com>
2024-08-08 10:41:41 -0300

    dfsf

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
